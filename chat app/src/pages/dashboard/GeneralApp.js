a223e93f8897283fab63fcd87dce43dc77c2140b
Aishez singh <aishezsingh@gmail.com>
2023-10-26 23:13:12 +0530

    Video 1 : Navbar

---
+++ chat app/src/pages/dashboard/GeneralApp.js
@@ -0,0 +1,13 @@
+import React ,{Suspense , lazy}  from "react";
+
+const GeneralApp = () => {
+
+    return (
+        <>
+            App
+        </>
+    );
+};
+
+export default GeneralApp;
+    
