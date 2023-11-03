0803da506dc973beeee194c8fe6e2922f91aae74
AshWankhede <117411585+AshWankhede@users.noreply.github.com>
2023-11-03 10:44:34 +0530

    Vid 6 done till 23:07

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -2,7 +2,7 @@ import { Stack, Box } from "@mui/material";
 import Chats from "./Chats";
 import Conversation from "../../components/Conversation";
 import { useTheme } from "@mui/material/styles";
-
+import Contact from "../../components/Conversation/Contact";
 const GeneralApp = () => {
     const theme = useTheme();
     return (
@@ -10,10 +10,10 @@ const GeneralApp = () => {
 
             <Chats />
 
-            <Box sx={{ height: "100%", width: "calc(100vw - 420px)", backgroundColor: theme.palette.mode === "light" ? "#F0F4FA"  : theme.palette.background.paper }}>
+            <Box sx={{ height: "100%", width: "calc(100vw - 740px)", backgroundColor: theme.palette.mode === "light" ? "#F0F4FA"  : theme.palette.background.paper }}>
                 <Conversation />
             </Box>
-
+            <Contact/>
         </Stack>
     );
 };
